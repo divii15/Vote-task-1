6f536cb7bb8db009db730a6106f0a2b2b591de6b
divii15 <divyadaksha143@gmail.com>
2025-01-06 16:54:42 +0530

    My next task(1)(5)

--- Task 1 Vote.py
+++ Task 1 Vote.py
@@ -1,3 +1,11 @@
+#TASK 1
+#5)Vote Eligible?
+#input:12
+#output:not eligible for vote
+#input:19
+#output:eligible for vote
+
+
 age = int(input("Enter the number: "))
 if age>=18 :
     print("Eligible for vote")
